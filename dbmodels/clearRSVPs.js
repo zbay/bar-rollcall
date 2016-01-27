fd952b89fa582a0ee4518e0b5c614ef3ee1c47ab
zbay <zlwilliams1@gmail.com>
2016-01-27 02:35:52 +0000

    Procfile and package.json fixes

--- dbmodels/clearRSVPs.js
+++ dbmodels/clearRSVPs.js
@@ -1,8 +1,8 @@
 var mongoose = require("mongoose");
 var Attendance = require("./attendance");
 
-mongoose.connect('mongodb://localhost:27017/bar-rollcall', function (err, db)
-//mongoose.connect(process.env.MONGOLAB_URI, function (err, db)
+//mongoose.connect('mongodb://localhost:27017/bar-rollcall', function (err, db)
+mongoose.connect(process.env.MONGOLAB_URI, function (err, db)
 {
     console.log("connected?");
 Attendance.remove({}, function(error, msg){
